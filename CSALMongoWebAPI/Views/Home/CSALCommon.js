93d762d7caec7910b7e316091732476cc7dd0533
Craig Kelly <craig.n.kelly@gmail.com>
2014-09-09 16:30:28 -0500

    common js file

---
+++ CSALMongoWebAPI/Views/Home/CSALCommon.js
@@ -0,0 +1,41 @@
+﻿//NOTE: requires jQuery to be previously imported
+
+var CSALCommon = {
+    showRows: function (tableSelector, rows) {
+        var table = $(tableSelector).DataTable();
+        table
+            .clear()
+            .rows.add(rows)
+            .draw()
+        ;
+    },
+
+    doServerGet: function (url, dataTarget) {
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: "json"
+        })
+        .done(function (data, textStatus, jqXHR) {
+            console.log("Call done [" + url + "]: " + textStatus);
+            if (textStatus != "success") {
+                //OOPS - server didn't like our request
+                var errMsg = textStatus + "???";
+                try {
+                    errMsg = data.errmsg;
+                } catch (err) { }
+                alert("There was an issue [" + url + "]: " + errMsg);
+            }
+            else {
+                //SUCCESS!
+                dataTarget(data);
+            }
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("Call done [" + url + "]: " + textStatus + ", error:" + errorThrown);
+            alert("There was an error [" + url + "] ==> " +
+                "[" + textStatus + ": " + errorThrown + "]"
+            );
+        });
+    }
+};

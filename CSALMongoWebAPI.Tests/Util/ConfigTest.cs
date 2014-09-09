5fb3a099ecbfd374434858b1d33da49826bdd66c
Craig Kelly <craig.n.kelly@gmail.com>
2014-09-09 15:29:51 -0500

    Include testing for AppSettings config

---
+++ CSALMongoWebAPI.Tests/Util/ConfigTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CSALMongoWebAPI;
+using CSALMongoWebAPI.Controllers;
+using CSALMongo;
+using CSALMongo.Model;
+
+namespace CSALMongoWebAPI.Tests.Util {
+    [TestClass]
+    public class ConfigTest: BaseControllerTest {
+        [TestMethod]
+        public void TestAppSettingChanges() {
+            var controller = new TurnController();
+            this.AppSettings.Add(controller.AppSettings);
+            Assert.IsFalse(String.IsNullOrEmpty(this.AppSettings["MongoURL"]));
+        }
+    }
+}

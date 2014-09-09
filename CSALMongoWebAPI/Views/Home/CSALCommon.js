432c057e139000dcb925cbd55965e39b8453f2f8
Craig Kelly <craig.n.kelly@gmail.com>
2014-09-09 18:15:48 -0500

    web api centralized progress display

--- CSALMongoWebAPI/Views/Home/CSALCommon.js
+++ CSALMongoWebAPI/Views/Home/CSALCommon.js
@@ -1,6 +1,47 @@
 ﻿//NOTE: requires jQuery to be previously imported
 
+//Helper for jQuery to see if a query returns anything
+$.fn.exists = function () {
+    return this.length && this.length !== 0;
+};
+
+//Actual helpers
 var CSALCommon = {
+    progressDialogSelector: null,
+    progressDialogTextSelector: null,
+
+    showProgress: function (txt) {
+        try {
+            if (!!CSALCommon.progressDialogSelector) {
+                var dlg = $(CSALCommon.progressDialogSelector);
+                if (dlg.exists()) {
+                    if (!!CSALCommon.progressDialogTextSelector) {
+                        console.log(txt);
+                        $(CSALCommon.progressDialogTextSelector).text(!!txt ? txt : "Working");
+                    }
+                    dlg.show();
+                }
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    },
+
+    hideProgress: function () {
+        try {
+            if (!!CSALCommon.progressDialogSelector) {
+                var dlg = $(CSALCommon.progressDialogSelector);
+                if (dlg.exists()) {
+                    dlg.hide();
+                }
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    },
+
     showRows: function (tableSelector, rows) {
         var table = $(tableSelector).DataTable();
         table
@@ -10,13 +51,15 @@ var CSALCommon = {
         ;
     },
 
-    doServerGet: function (url, dataTarget) {
+    doServerGet: function (url, progressText, dataTarget) {
+        CSALCommon.showProgress(progressText);
         $.ajax({
             type: "GET",
             url: url,
             dataType: "json"
         })
         .done(function (data, textStatus, jqXHR) {
+            CSALCommon.hideProgress();
             console.log("Call done [" + url + "]: " + textStatus);
             if (textStatus != "success") {
                 //OOPS - server didn't like our request
@@ -32,6 +75,7 @@ var CSALCommon = {
             }
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
+            CSALCommon.hideProgress();
             console.log("Call done [" + url + "]: " + textStatus + ", error:" + errorThrown);
             alert("There was an error [" + url + "] ==> " +
                 "[" + textStatus + ": " + errorThrown + "]"

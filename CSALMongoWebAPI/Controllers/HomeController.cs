5c3f3489927dce14c2049112a9f49d82e9840fa2
Craig Kelly <craig.n.kelly@gmail.com>
2014-09-11 15:54:33 -0500

    web api ui - skeleton for detail views

--- CSALMongoWebAPI/Controllers/HomeController.cs
+++ CSALMongoWebAPI/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CSALMongo;
+
 namespace CSALMongoWebAPI.Controllers {
     /// <summary>
     /// Our Home controller is actually where we render the active HTML5 web
@@ -21,12 +23,25 @@ namespace CSALMongoWebAPI.Controllers {
             return View();
         }
 
+        public ActionResult ClassDetails(string id) {
+            return View("ClassDetail", new ClassesController().Get(id));
+        }
+
         public ActionResult Lessons() {
             return View();
         }
 
+        public ActionResult LessonDetails(string id) {
+            var lesson = new CSALMongo.Model.Lesson();
+            return View("LessonDetail", new LessonsController().Get(id));
+        }
+
         public ActionResult Students() {
             return View();
+        }
+
+        public ActionResult StudentDetails(string id) {
+            return View("StudentDetail", new StudentsController().Get(id));
         }
     }
 }

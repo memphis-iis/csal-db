5ad85e6848296ace5006d175e237b6c30ba29b7b
Craig Kelly <craig.n.kelly@gmail.com>
2014-09-12 11:46:17 -0500

    students at location web api

--- CSALMongo/CSALDatabase.cs
+++ CSALMongo/CSALDatabase.cs
@@ -9,7 +9,6 @@ using MongoDB.Driver.Builders;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 
-//TODO: get student info by location
 //TODO: Change index page to have instructions, move current index to a test page
 //TODO: class detail is list of lessons?
 
---
+++ CSALMongoWebAPI/Controllers/StudentsAtLocationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using CSALMongo.Model;
+
+namespace CSALMongoWebAPI.Controllers {
+    public class StudentsAtLocationController : Util.CSALBaseController {
+        // GET api/students/5
+        public List<Student> Get(string id) {
+            return DBConn().FindStudentsByLocation(id);
+        }
+    }
+}

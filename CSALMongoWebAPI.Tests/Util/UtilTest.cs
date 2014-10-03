e2a4a5593487e3c20b0235001e5adbaf192509f6
Craig Kelly <craig.n.kelly@gmail.com>
2014-10-03 16:39:47 -0500

    test enhancement

--- CSALMongoWebAPI.Tests/Util/UtilTest.cs
+++ CSALMongoWebAPI.Tests/Util/UtilTest.cs
@@ -19,7 +19,9 @@ namespace CSALMongoWebAPI.Tests.Util {
 
         [TestMethod]
         public void HumanDuration() {
-            Assert.AreEqual("< 1 min", RenderHelp.HumanDuration(0.0));
+            Assert.AreEqual("", RenderHelp.HumanDuration(-1.0));
+            Assert.AreEqual("", RenderHelp.HumanDuration(0.0));
+            Assert.AreEqual("< 1 min", RenderHelp.HumanDuration(2.0));
             Assert.AreEqual("2 mins", RenderHelp.HumanDuration(129.2 * 1000.0));
             Assert.AreEqual("4 hrs", RenderHelp.HumanDuration(4.1 * 60 * 60 * 1000.0));
         }
